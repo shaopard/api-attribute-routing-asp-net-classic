591f2763a82ad81f18b675476833a3f97eacaa5f
DESKTOP-ICH4FRR\Adrian Buda <adrian.buda.dev@gmail.com>
2022-12-06 16:17:15 +0200

    Initial commit.

---
+++ WebApplication1-Versioning/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("values/get/v1")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("values/get/v2")]
+        public IEnumerable<string> Getv2()
+        {
+            return new string[] { "value3", "value4" };
+        }
+    }
+}
